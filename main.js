9e41b3035a5e19e0c77ac271d2a49839da768516
almero0922 <albertomedel29@gmail.com>
2022-08-17 18:31:01 -0500

    Active States, missing inactive states

--- main.js
+++ main.js
@@ -6,9 +6,22 @@ const rePass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 caSubmit.addEventListener('click', (el) => {
     //let isInputCorrect = checkInfo(fData);
-    for(let i = 0; i < f.length; i++){
+    for(let i = 0; i < f.length-1; i++){
         console.log(f[i].value);
         console.log(checkInfo(f[i].value, f[i].getAttribute('type')));
+        let type = f[i].getAttribute('type');
+        let p = document.createElement('p');
+        let placeHolder = f[i].getAttribute('placeholder');
+        if(!checkInfo(f[i].value, type)){
+            f[i].classList.add('input-error');
+            p.classList.add('text-error');
+            if(type == 'text' || type == 'password'){
+                p.innerText = placeHolder + ' cannot be empty or is wrong';
+            }else if(type == 'email'){
+                p.innerText = 'Looks like this is not an email';
+            }
+            f[i].parentNode.insertBefore(p, f[i+1]);
+        }
     }
     el.preventDefault();
 })
