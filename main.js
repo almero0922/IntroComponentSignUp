e2e72e01c06196494b0c241c607dfc9fefd24bf7
almero0922 <albertomedel29@gmail.com>
2022-08-18 19:32:43 -0500

    Almost done Js logic

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const f = document.forms['formA'].elements;
 const caSubmit = document.querySelector('.input-submit');
-const reText = /^[a-zA-Z\s]*$/;
+const reText = /^[a-zA-Z\s]{1,10}/;
 const reEmail = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
 const rePass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 caSubmit.addEventListener('click', (el) => {
     //let isInputCorrect = checkInfo(fData);
     for(let i = 0; i < f.length-1; i++){
-        console.log(f[i].value);
-        console.log(checkInfo(f[i].value, f[i].getAttribute('type')));
         let type = f[i].getAttribute('type');
         let p = document.createElement('p');
         let placeHolder = f[i].getAttribute('placeholder');
-        if(!checkInfo(f[i].value, type)){
+        let validation = checkInfo(f[i].value, type);
+        if(!validation){
             f[i].classList.add('input-error');
             p.classList.add('text-error');
             if(type == 'text' || type == 'password'){
@@ -21,6 +20,12 @@ caSubmit.addEventListener('click', (el) => {
                 p.innerText = 'Looks like this is not an email';
             }
             f[i].parentNode.insertBefore(p, f[i+1]);
+        }else{
+            let nextError = f[i].nextElementSibling;
+            if(nextError.tagName == 'P'){
+                nextError.parentNode.removeChild(nextError);
+                f[i].classList.remove('input-error');
+            }
         }
     }
     el.preventDefault();
